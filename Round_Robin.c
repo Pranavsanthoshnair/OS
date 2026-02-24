aefbd7013303c175c578da0aefa5cdc3d83b9e0a
Pranav S Nair <pranavsanthoshnair@gmail.com>
2026-02-24 12:34:49 +0530

    Refactor Round Robin scheduling code for clarity

--- Round_Robin.c
+++ Round_Robin.c
@@ -1,32 +1,38 @@
 #include<stdio.h>
 
 int main(){
+
     int n, tq;
+
     printf("Enter No of Processes: ");
     scanf("%d",&n);
 
-    int AT[n], BT[n], RT[n], WT[n], TAT[n], CT[n];
-    int total_WT = 0, total_TAT = 0;
+    int AT[n], BT[n], RT[n];
+    int WT[n], TAT[n], CT[n];
 
     for(int i=0;i<n;i++){
-        printf("Enter Arrival Time of process %d: ",i+1);
+        printf("\nProcess %d\n",i+1);
+        printf("Arrival Time: ");
         scanf("%d",&AT[i]);
-        printf("Enter Burst Time of process %d: ",i+1);
+        printf("Burst Time: ");
         scanf("%d",&BT[i]);
         RT[i] = BT[i];
     }
 
-    printf("Enter Time Quantum: ");
+    printf("\nEnter Time Quantum: ");
     scanf("%d",&tq);
 
-    int completed = 0, time = 0;
+    int time = 0, completed = 0;
 
     while(completed < n){
-        int done_flag = 1;
+
+        int executed = 0;
 
         for(int i=0;i<n;i++){
-            if(RT[i] > 0 && AT[i] <= time){
-                done_flag = 0;
+
+            if(AT[i] <= time && RT[i] > 0){
+
+                executed = 1;
 
                 if(RT[i] > tq){
                     time += tq;
@@ -40,26 +46,28 @@ int main(){
 
                     TAT[i] = CT[i] - AT[i];
                     WT[i] = TAT[i] - BT[i];
-
-                    total_WT += WT[i];
-                    total_TAT += TAT[i];
                 }
             }
         }
 
-        if(done_flag == 1){
+        if(executed == 0)
             time++;
-        }
     }
 
-    printf("\nProcess  AT  BT  WT  TAT\n");
+    float totalWT = 0, totalTAT = 0;
+
+    printf("\nProcess AT BT WT TAT\n");
+
     for(int i=0;i<n;i++){
-        printf("%d        %d   %d   %d   %d\n",
-               i+1,AT[i],BT[i],WT[i],TAT[i]);
+        printf("%d      %d  %d  %d  %d\n",
+               i+1, AT[i], BT[i], WT[i], TAT[i]);
+
+        totalWT += WT[i];
+        totalTAT += TAT[i];
     }
 
-    printf("Avg WT = %.2f\n",(float)total_WT/n);
-    printf("Avg TAT = %.2f",(float)total_TAT/n);
+    printf("\nAverage Waiting Time = %.2f\n", totalWT/n);
+    printf("Average Turnaround Time = %.2f\n", totalTAT/n);
 
     return 0;
 }
